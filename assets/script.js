21baf8cbbd6e7e7f454a71ad5fbdbf5267ab450e
ishelly1997 <ishelly1997@gmail.com>
2022-01-16 23:54:29 -0500

    final

--- assets/script.js
+++ assets/script.js
@@ -12,118 +12,114 @@ var li4 = document.createElement('li');
 
 
 
-startBtn.addEventListener('click', startQuiz)
-(function(){
-        instructionsEl.setAttribute("style", "hidden")
-        // Functions
-    
-        function startQuiz(){
-          // variable to store the HTML output
-          const output = [];
-      
-          // for each question...
-          myQuestions.forEach(
-            (currentQuestion, questionNumber) => {
-      
-              // variable to store the list of possible answers
-              const answers = [];
-      
-              // and for each available answer...
-              for(letter in currentQuestion.answers){
-      
-                // ...add an HTML radio button
-                answers.push(
-                  `<label>
-                    <input type="button" name="question${questionNumber}" value="${currentQuestion.answers[letter]}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                  </label>`
+(function start(){
+
+        function startQuiz() {
+
+            instructionsEl.setAttribute('style', 'hidden');
+
+            var output = [];
+        
+            myQuestions.forEach(
+              (currentQuestion, questionNumber) => {
+        
+
+                var answers = [];
+
+                for(letter in currentQuestion.answers){
+        
+
+                  answers.push(
+                    `<label>
+                      <input class="multi" type="radio" name="question${questionNumber}" value="${letter}">
+                      ${letter} :
+                      ${currentQuestion.answers[letter]}
+                    </label>`
+                  );
+                }
+        
+  
+                output.push(
+                  `<div class="step">
+                    <div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join("")} </div>
+                  </div>`
                 );
               }
-      
-              // add this question and its answers to the output
-              output.push(
-                `<div class="slide">
-                  <div class="question"> ${currentQuestion.question} </div>
-                  <div class="answers"> ${answers.join("")} </div>
-                </div>`
-              );
-            }
-          );
-      
-          // finally combine our output list into one string of HTML and put it on the page
-          quizContainer.innerHTML = output.join('');
-        }
+            );
+        
+
+            quizContainer.innerHTML = output.join('');
+            function showQuestion(n) {
+                slides[thisQuestion].classList.remove('current-question');
+                slides[n].classList.add('current-question');
+                thisQuestion = n;
+                console.log('66');
+
+                //var multiBtn = document.querySelectorAll('.multi');
+                //multiBtn.addEventListener('click', nextq);
+              }
+              console.log('12');
+              function nextq() {
+                showQuestion(thisQuestion + 1);
+              }
+              console.log('34');
+            const slides = document.querySelectorAll(".step");
+            let thisQuestion = 0;
+            
+
+console.log('78');
+showQuestion(thisQuestion);
+//const multiBtn = quizContainer.querySelectorAll('.multi');
+
+document.querySelector('body').addEventListener('click', function(event) {
+    if (event.target.tagName.toLowerCase() === 'input') {
+      console.log('cow');
+      nextq();
+      showResults();
+    }
+  });
+//showResults();
+          }
+
       
         function showResults(){
       
-          // gather answer containers from our quiz
           const answerContainers = quizContainer.querySelectorAll('.answers');
       
-          // keep track of user's answers
+
           let numCorrect = 0;
       
-          // for each question...
+          console.log('99');
           myQuestions.forEach( (currentQuestion, questionNumber) => {
       
-            // find selected answer
             const answerContainer = answerContainers[questionNumber];
             const selector = `input[name=question${questionNumber}]:checked`;
             const userAnswer = (answerContainer.querySelector(selector) || {}).value;
       
-            // if answer is correct
-            if(userAnswer === currentQuestion.correctAnswer){
-              // add to the number of correct answers
+            if(userAnswer === currentQuestion.right){
+
               numCorrect++;
-      
-              // color the answers green
+
               answerContainers[questionNumber];
             }
-            // if answer is wrong or blank
             else{
-              // color the answers red
-              answerContainers[questionNumber].style.color = 'red';
+
+              answerContainers[questionNumber];
             }
+            console.log('88');
+            //selector.addEventListener('click', nextq);
+            //nextq();
           });
       
-          // show number of correct answers out of total
-          resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+          resultsContainer.innerHTML = `${numCorrect} / ${myQuestions.length}`;
         }
-      
-        function showSlide(n) {
-          slides[currentSlide].classList.remove('active-slide');
-          slides[n].classList.add('active-slide');
-          currentSlide = n;
-          if(currentSlide === 0){
-            previousButton.style.display = 'none';
-          }
-          else{
-            previousButton.style.display = 'inline-block';
-          }
-          if(currentSlide === slides.length-1){
-            nextButton.style.display = 'none';
-            submitButton.style.display = 'inline-block';
-          }
-          else{
-            nextButton.style.display = 'inline-block';
-            submitButton.style.display = 'none';
-          }
-        }
-      
-        function showNextSlide() {
-          showSlide(currentSlide + 1);
-        }
-      
-        function showPreviousSlide() {
-          showSlide(currentSlide - 1);
-        }
-      
-        // Variables
-        const quizContainer = document.getElementById('quiz');
-        const resultsContainer = document.getElementById('results');
+        console.log('77');
+
         const submitButton = document.getElementById('submit');
     
-      
+        console.log('56');
         var myQuestions = [
             {
                 question: 'The condition in an if/else statement is enclosed with ',
@@ -133,7 +129,7 @@ startBtn.addEventListener('click', startQuiz)
                     c : '3.	parenthesis',
                     d : '4.	square brackets'
                 },
-                correctAnswer: 'b'
+                right: 'b'
             },
             {
                 question: 'Arrays in Javascript can be used to store ',
@@ -143,7 +139,7 @@ startBtn.addEventListener('click', startQuiz)
                     c : '3.	booleans',
                     d : '4.	all of the above'
                 },
-                correctAnswer: 'd'
+                right: 'd'
             },
             {
                 question: 'String values must be enclosed within _____ when being assigned to variables ',
@@ -153,7 +149,7 @@ startBtn.addEventListener('click', startQuiz)
                     c : '3.	quotes',
                     d : '4.	parenthesis'
                 },
-                correctAnswer: 'c'
+                right: 'c'
             },
             {
                 question: 'A very useful tool used during development and debugging for printing content to the debugger is ',
@@ -163,20 +159,18 @@ startBtn.addEventListener('click', startQuiz)
                     c : '3.	for loops',
                     d : '4.	console.log'
                 },
-                correctAnswer: 'd'
+                right: 'd'
             }
         ];
-        // Kick things off
-        startQuiz();
-      
-        // Pagination
-        const slides = document.querySelectorAll(".slide");
-        let currentSlide = 0;
-      
-        // Show the first slide
-        showSlide(currentSlide);
-      
-        // Event listeners
-        submitButton.addEventListener('click', showResults);
-      })();
-      
+
+        //startQuiz();
+        startBtn.addEventListener('click', startQuiz);
+        //submitButton.addEventListener('click', showResults);
+        //const multiBtn = document.querySelectorAll('.multi');
+        //multiBtn.addEventListener('click', showResults);
+
+
+
+      }
+    
+)()
